e0bc32d5c3f1584b95412b0211037c3d83d9309e
Rewind <sploing1@gmail.com>
2018-07-21 21:36:35 -0700

    Added menu swapping MenuItem generation and EditorUtils

---
+++ Assets/Editor/Generated/GMenuSwapMenuItems.cs
@@ -0,0 +1,21 @@
+﻿/** This class is Auto-Generated **/
+
+using UnityEditor;
+
+namespace Editor.Generated
+{
+    public static class GMenuSwapMenuItems {
+
+    [MenuItem("Menu/MainMenu")]
+    private static void SwapToMainMenu() => MenuSwap.SwapMenu("MainMenu");
+    [MenuItem("Menu/MainMenu", true)]
+    private static bool SwapToMainMenuValidation() => MenuSwap.SwapMenuValidation("MainMenu");
+
+    [MenuItem("Menu/OtherMenu")]
+    private static void SwapToOtherMenu() => MenuSwap.SwapMenu("OtherMenu");
+    [MenuItem("Menu/OtherMenu", true)]
+    private static bool SwapToOtherMenuValidation() => MenuSwap.SwapMenuValidation("OtherMenu");
+
+
+    }
+}
